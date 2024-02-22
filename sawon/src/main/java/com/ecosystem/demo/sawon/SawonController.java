6e0600c8382085c80fa05db42e399779f9ab1cbc
jeong jayun <1252564@gmail.com>
2024-02-22 16:43:19 +0900

    20240222

--- sawon/src/main/java/com/ecosystem/demo/sawon/SawonController.java
+++ sawon/src/main/java/com/ecosystem/demo/sawon/SawonController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,28 +24,46 @@ public class SawonController {
 
 	@GetMapping("/")
 	public String index() {
-		return "redirect:/list";
+		return "index";
 	}
-
-	@GetMapping("/list")
-	public String getSawonList(Model model) {
-		logger.info("getSawonList 실행");
+	
+	//thymleaf
+	@GetMapping("/thymeleafList")
+	public String getThymeleafList(Model model) {
 		List<SawonDTO> sawonList = sawonService.getSawonList();
 		model.addAttribute("sawonList", sawonList);
-		return "sawonList";
+		return "thymeleafList";
 	}
-
+	
 	@GetMapping("/register")
 	public String getRegister(Model model) {
 		logger.info("사원 등록 페이지로 이동");
 		model.addAttribute("sawon", new SawonDTO());
 		return "register_form";
 	}
-
+	
 	@PostMapping("/register")
 	public String postRegister(SawonDTO sawonDTO) {
 		logger.info("postRegister 실행");
 		sawonService.register(sawonDTO);
-		return "redirect:/list";
+		return "redirect:/";
 	}
+
+	// jqgrid
+	@GetMapping("/jqGrid")
+	public String getJqGridList(Model model) {
+		List<SawonDTO> sawonList = sawonService.getSawonList();
+		model.addAttribute("sawonList", sawonList);
+		return "jqGrid";
+	}
+	
+	@RequestMapping(value = "/jsonGetSawonList", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public List<SawonDTO> jqList(SawonDTO sawonDto, Model model) throws Exception {
+		List<SawonDTO> sawonList = sawonService.getSawonList();
+		logger.info("jqGrid에서 url : /jsonGetSawonList 실행 : " + sawonList);
+		model.addAttribute("list", sawonList);
+		return sawonList;
+	}
+
 }
